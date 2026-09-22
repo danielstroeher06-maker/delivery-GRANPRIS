206bf4bf90ec76433340f9f996b138e9808296da
delivery-GRANPRIS <danielstroeher06@gmail.com>
2026-09-21 21:32:18 -0300

    Avança integração entre Service e Repository

---
+++ src/main/java/com/vinicola/deliveryvinicola/repository/VinhoRepository.java
@@ -0,0 +1,8 @@
+package com.vinicola.deliveryvinicola.repository;
+
+import com.vinicola.deliveryvinicola.model.Vinho;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VinhoRepository extends JpaRepository <Vinho, Long> {
+
+}
--- src/main/java/com/vinicola/deliveryvinicola/service/VinhoService.java
+++ src/main/java/com/vinicola/deliveryvinicola/service/VinhoService.java
@@ -1,23 +1,54 @@
 package com.vinicola.deliveryvinicola.service;
 
+import com.vinicola.deliveryvinicola.repository.VinhoRepository;
 import org.springframework.stereotype.Service;
 import com.vinicola.deliveryvinicola.model.Vinho;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class VinhoService {
-    public void vender(Vinho vinho, int quantidade) {
-        int estoque = vinho.getEstoque();
 
-        if (quantidade <= 0) {
-            System.out.println("Quantidade invalida!");
-            return;
-        }
+    private VinhoRepository repository;
 
-        if (quantidade > estoque) {
-            System.out.println("Quantidade invalida!");
-            return;
+    public VinhoService(VinhoRepository repository) {
+        this.repository = repository;
+    }
+
+    public void vender(Long id, int quantidade) {
+
+        Optional<Vinho> Optionalvinho = repository.findById(id);
+
+        if (Optionalvinho.isPresent()) {
+            Vinho vinho = Optionalvinho.get();
+
+            int estoque = vinho.getEstoque();
+
+            if (quantidade > estoque) {
+                System.out.println("Quantidade invalida!");
+                return;
+            }
+
+            if (quantidade <= 0) {
+                System.out.println("Quantidade invalida!");
+                return;
+            }
+
+            vinho.setEstoque(vinho.getEstoque() - quantidade);
+            repository.save(vinho);
+
+            System.out.println("Compra realizada com sucesso!");
+
+        } else {
+            System.out.println("Vinho n encontrado no estoque");
         }
-        vinho.setEstoque(estoque - quantidade);
-        System.out.println("Compra realizada com sucesso!");
+    }
+
+    public List<Vinho> listarVinhos() {
+
+        List<Vinho> vinhos = repository.findAll();
+        return vinhos;
+
     }
 }
